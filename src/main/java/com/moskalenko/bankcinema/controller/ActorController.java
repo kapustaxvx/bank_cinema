8f4b2eeb417cb90edcb4f2c55f99ff15b308e48f
a19190480 <IIMoskalenko@sberbank.ru>
2021-06-23 13:47:21 +0300

    update swagger info

--- src/main/java/com/moskalenko/bankcinema/controller/ActorController.java
+++ src/main/java/com/moskalenko/bankcinema/controller/ActorController.java
@@ -4,6 +4,7 @@ import com.moskalenko.bankcinema.api.DTO.ActorDTO;
 import com.moskalenko.bankcinema.api.client.ActorClient;
 import com.moskalenko.bankcinema.api.entity.Actor;
 import com.moskalenko.bankcinema.service.ActorService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/actors")
+@RequestMapping("api/actors")
 public class ActorController implements ActorClient {
     private final ActorService actorService;
 
@@ -26,12 +27,14 @@ public class ActorController implements ActorClient {
 
     @Override
     @PostMapping
+    @ApiOperation(value = "Add new actor")
     public Actor addActor(@RequestBody ActorDTO actorData) {
         return actorService.addActor(actorData);
     }
 
     @Override
     @PutMapping("/{actorId}/movies/{movieId}")
+    @ApiOperation(value = "Add movie in the actor's list")
     public @ResponseBody String addActorToMovie(@PathVariable Long actorId, @PathVariable Long movieId) {
         actorService.addActorToMovie(actorId, movieId);
         return "Movie added";
@@ -39,6 +42,7 @@ public class ActorController implements ActorClient {
 
     @Override
     @GetMapping("/{actorId}")
+    @ApiOperation(value = "Get actor by id")
     public Actor getActorById(@PathVariable Long actorId) {
         return actorService.getActorById(actorId);
     }
@@ -46,6 +50,7 @@ public class ActorController implements ActorClient {
 
     @Override
     @GetMapping
+    @ApiOperation("Get list of all actors")
     public Collection<Actor> getAllActors() {
         return actorService.getAllActors();
     }
--- src/main/java/com/moskalenko/bankcinema/controller/MovieController.java
+++ src/main/java/com/moskalenko/bankcinema/controller/MovieController.java
@@ -4,6 +4,7 @@ import com.moskalenko.bankcinema.api.DTO.MovieDTO;
 import com.moskalenko.bankcinema.api.client.MovieClient;
 import com.moskalenko.bankcinema.api.entity.Movie;
 import com.moskalenko.bankcinema.service.MovieService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("api/movies")
 public class MovieController implements MovieClient {
 
     private final MovieService movieService;
@@ -25,18 +26,21 @@ public class MovieController implements MovieClient {
 
     @Override
     @PostMapping
+    @ApiOperation(value = "Add new movie")
     public Movie addMovie(@RequestBody MovieDTO movieData) {
         return movieService.addMovie(movieData);
     }
 
     @Override
     @GetMapping("/{movieId}")
+    @ApiOperation(value = "Get movie by id")
     public Movie getMovieById(@PathVariable Long movieId) {
         return movieService.getMovieById(movieId);
     }
 
     @Override
     @GetMapping
+    @ApiOperation(value = "Get list of all movies")
     public Collection<Movie> getAllMovies() {
         return movieService.getAllMovies();
     }
